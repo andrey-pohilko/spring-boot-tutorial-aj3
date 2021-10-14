c8572a07f13a3de7bf65af6c1c99585787b8b090
Andrei Pokhilko <andrei.pokhilko@aspekt.com.mk>
2021-10-14 21:00:52 +0600

    fix commands

--- scripts/docker/deliver.cmd
+++ scripts/docker/deliver.cmd
@@ -7,12 +7,12 @@ scp ..\..\target\spring-boot-tutorial-aj3-0.0.1-SNAPSHOT.jar ^
 scp Dockerfile root@49.12.229.158:/var/lib/spring-boot-tutorial-aj3/
 
 
-ssh root@49.12.229.158 "docker kill spring-boot-tutorial-aj3"
+ssh root@49.12.229.158 "docker stop spring-boot-tutorial-aj3"
+ssh root@49.12.229.158 "docker rm spring-boot-tutorial-aj3"
+
 
 @rem build docker with tag
-ssh root@49.12.229.158 ^
-        cd /var/lib/spring-boot-tutorial-aj3 && ^
-        docker build -t spring-boot-tutorial-aj3-image .
+ssh root@49.12.229.158 "cd /var/lib/spring-boot-tutorial-aj3 && docker build -t anoxis/spring-boot-tutorial-aj3-image ."
 
 @rem -t - assign tag spring-boot-tutorial-aj3-image to the built image
 
@@ -20,7 +20,7 @@ ssh root@49.12.229.158 ^
 @rem run docker with port 9000 assigned to port 9000 and in background
 ssh root@49.12.229.158 ^
         docker run --restart unless-stopped -d -p 9000:9000 ^
-                   --name spring-boot-tutorial-aj3 spring-boot-tutorial-aj3-image
+                   --name spring-boot-tutorial-aj3 anoxis/spring-boot-tutorial-aj3-image
 
 @rem --restart unless-stopped - restart always unless its stopped manually by a command
 @rem -d run in background
